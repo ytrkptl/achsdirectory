59159de3a12442eb43211154291a9f0ea114a21d
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-02-07 18:05:44 -0500

    change robot images src url

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-const Card  = ({ requestIndiCard, lastname, firstname, email, phone, url, itemNum}) => {
+const Card = ({ requestIndiCard, lastname, firstname, email, phone, url, itemNum }) => {
 	let name = firstname + ' ' + lastname;
 	return (
-		<div className='CardDetails growCard2' onClick={()=>requestIndiCard(true, itemNum)}>
+		<div className='CardDetails growCard2' onClick={() => requestIndiCard(true, itemNum)}>
 			<div className='imgContainer'>
-				<img className="imgRounded" alt='robots' src={`https://robohash.org/${firstname + lastname}?size=133x170&set=set2&bgset=bg1`} />
+				<img className="imgRounded" alt='robots' src={url} />
 			</div>
 			<div className="textContainer">
 				<h3 className="subjectTitle">{name}</h3>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import './Header.css';
 import SearchBox from './SearchBox';
 import ToggleButton from './ToggleButton';
+import './Header.css';
 
-const Header = ({searchChange}) => {
+const Header = ({ searchChange }) => {
 	return (
-			<div className="headerContainer">
-				<div className="row1">
-					<a href="/" style={{textDecoration: "none"}}><h1 className='makeFontBigger2'>ACHS Directory</h1></a>
-					<p className="phoneLabel">123-456-ext.</p>
-				</div>
-				<div className="searchSwitchContainer"> 
-		            <SearchBox searchChange={searchChange} />
-		            <p className="phoneLabel2">123-456-ext.</p>
-		            <ToggleButton />
-			    </div>
+		<div className="headerContainer">
+			<div className="row1">
+				<a href="/" style={{ textDecoration: "none" }}><h1 className='makeFontBigger2'>ACHS Directory</h1></a>
+				<p className="phoneLabel">123-456-ext.</p>
 			</div>
+			<div className="searchSwitchContainer">
+				<SearchBox searchChange={searchChange} />
+				<p className="phoneLabel2">123-456-ext.</p>
+				<ToggleButton />
+			</div>
+		</div>
 	);
 }
 
