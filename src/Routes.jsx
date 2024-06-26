adc939f09c53fb0eee8a551ee6179fa59dd14eb6
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-06-26 19:20:23 -0400

    chore: Update Routes.jsx to use new file path for Home component

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,14 +1,11 @@
-/* eslint-disable react/prop-types */
 import { Route, Routes, Outlet } from "react-router-dom";
-// import AdminSignIn from "./components/Admin/AdminSignIn";
-import Home from "./Home";
+import Home from "./pages/Home";
 
 const RoutesComponent = ({ searchfield }) => {
   return (
     <Routes>
       <Route element={<Outlet />} />
       <Route path="*" index element={<Home searchfield={searchfield} />} />
-      {/* <Route path="/admin" element={<AdminSignIn />} /> */}
     </Routes>
   );
 };
