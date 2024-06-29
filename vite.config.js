9c086f6a7c9cf92770f838f288eebcba1a29e40e
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-06-28 20:16:21 -0400

    Deleted postcss and autoprefixer; made use of createHtmlPlugin to dynamically update index.html with BASE_URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode,  '');
+  
+  // https://vitejs.dev/config/
+  return defineConfig({
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            BASE_URL: env.BASE_URL,
+          }
+        }
+      }),
+    ],
+    logLevel: 'info',
+    base: env.BASE_URL,
+    build: {
+      rollupOptions: {
+        input: './src/main.jsx',
+      }
+    },
+  })
+};
+
+
+// export default defineConfig({
+//   plugins: [react(),
+//     createHtmlPlugin({
+//       minify: true,
+//       inject: {
+//         data: {
+//           BASE_URL: base,
+//         }
+//       }
+//     })
+//   ],
+//   logLevel: 'info',
+//   base: '/',
+//   build: {
+//     rollupOptions: {
+//       input: './src/index.jsx',
+//     }
+//   },
+// })
