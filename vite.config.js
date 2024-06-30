9e0c21c8e50921cea9c001bb2126eea0940a2762
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-06-30 16:47:58 -0400

    chore: implement use of path aliases and delete the old data.json file.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import path from 'path'
 
 export default ({ mode }) => {
   // Load environment variables
@@ -26,26 +27,12 @@ export default ({ mode }) => {
         input: './src/main.jsx',
       }
     },
+    resolve: {
+      alias: {
+        // eslint-disable-next-line no-undef
+        "@": path.resolve(__dirname, './src'),
+        // Add more aliases here
+      }
+    },
   })
 };
-
-
-// export default defineConfig({
-//   plugins: [react(),
-//     createHtmlPlugin({
-//       minify: true,
-//       inject: {
-//         data: {
-//           BASE_URL: base,
-//         }
-//       }
-//     })
-//   ],
-//   logLevel: 'info',
-//   base: '/',
-//   build: {
-//     rollupOptions: {
-//       input: './src/index.jsx',
-//     }
-//   },
-// })
