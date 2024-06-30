dac6d5a3aabff80374b24fa50d487ef96fc43e85
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2024-06-30 16:41:49 -0400

    chore: Update import statement for App component in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 import { PrimeReactProvider } from "primereact/api";
+import App from "./App.jsx";
 
 const root = createRoot(document.getElementById("root"));
 
